55d36b2720bb960c6770ea22a78baed7edbee707
Csaba Koncz <csaba.koncz@casrd.net>
2014-06-17 15:56:20 +0200

    Replaced deprecated junit.framework usage

--- guice-examples/src/test/java/example/T08DefaultBindingsTest.java
+++ guice-examples/src/test/java/example/T08DefaultBindingsTest.java
@@ -1,8 +1,8 @@
 package example;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import java.util.logging.Logger;
 
